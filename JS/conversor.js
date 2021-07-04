dd4c68d85dcc699ca18a52951e3a30f85c706a3d
SidSan97 <sidnei1.8santiago@hotmail.com>
2021-07-04 15:08:25 -0300

    adição da função para converter caracteres

--- JS/conversor.js
+++ JS/conversor.js
@@ -1,2 +1,14 @@
 
-var texto = document.getElementById('texto');
+function convert() 
+{
+
+    var texto  = document.getElementById('texto').value;
+    var texto2 = document.getElementById('texto2');
+
+    texto2.value = "";
+
+    for (i=0; i < texto.length; i++) 
+    {
+        texto2.value +=texto[i].charCodeAt(0).toString(2) + " ";
+    }
+}

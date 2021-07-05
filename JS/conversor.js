069b64cdb4669de02f9208a9b2039872a93ec092
SidSan97 <sidnei1.8santiago@hotmail.com>
2021-07-05 10:39:10 -0300

    Adição de função para converter binario em decimal

--- JS/conversor.js
+++ JS/conversor.js
@@ -12,3 +12,19 @@ function convert()
         texto2.value +=texto[i].charCodeAt(0).toString(2) + " ";
     }
 }
+
+function convertToString()
+{
+    var texto  = document.getElementById('texto');
+    var texto2 = document.getElementById('texto2').value;
+
+    texto.value = "";
+
+    //texto2.replace(/ /g, "");
+    var decimal = parseInt(texto2, 2);
+    var dec = String.fromCharCode(decimal);
+
+    texto.value = dec;
+
+    alert(dec);
+}
